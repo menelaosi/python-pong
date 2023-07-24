71c83ca86cc033ff90918e1769679f64ab51d2c6
MenelaosI <menelaos.ioannou@gmail.com>
2023-07-24 15:53:59 -0400

    Adds Python Pong and instructions on using it.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-pong
 Testing Chat GPT code generation to demonstrate something basic but I've expanded it a little bit
+
+## Running this
+1. Clone this repository from github, download the python file, or just copy it over to your computer somehow
+2. Make sure you have Python installed on your machine (might be under python or python3 depending on setup)
+3. Run `python3 pong.py` or whatever Python command you have
+4. Use `w` and `s` for the left paddles; `up` and `down` for the right paddle
+
+## Next steps
+Going to add score-keeping to this if I decide to play with it more.
---
+++ pong.py
@@ -0,0 +1,100 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Pong")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=6, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=6, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(40)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
+
+running = True
+
+# Moving the paddles
+def paddle_a_up():
+    paddle_a.sety(paddle_a.ycor() + 20)
+
+def paddle_a_down():
+    paddle_a.sety(paddle_a.ycor() - 20)
+
+def paddle_b_up():
+    paddle_b.sety(paddle_b.ycor() + 20)
+
+def paddle_b_down():
+    paddle_b.sety(paddle_b.ycor() - 20)
+
+def exit_game():
+    global running
+    running = False
+
+# Keyboard bindings
+screen.listen()
+screen.onkeypress(paddle_a_up, "w")
+screen.onkeypress(paddle_a_down, "s")
+screen.onkeypress(paddle_b_up, "Up")
+screen.onkeypress(paddle_b_down, "Down")
+screen.onkeypress(exit_game, "Escape")
+
+# Main game loop
+while running:
+    screen.update()
+
+    # Ball movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border collision
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle collision
+    if (ball.dx > 0) and (350 > ball.xcor() > 340) and (paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50):
+        ball.color("blue")
+        ball.dx *= -1
+
+    elif (ball.dx < 0) and (-350 < ball.xcor() < -340) and (paddle_a.ycor() + 50 > ball.ycor() > paddle_a.ycor() - 50):
+        ball.color("red")
+        ball.dx *= -1
+
+    elif ball.xcor() > 390 or ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
